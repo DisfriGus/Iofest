930c5ab6dc33f35859b289d4abf1174edafc58e8
DisfriGus <dio.siregar03@gmail.com>
2023-04-30 09:45:32 +0700

    Update

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {HiOutlineArrowUpRight} from 'react-icons/hi2'
-import './Navbar.css'
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className=''>
         <div>
             <Link to='/'>
                 Logo
             </Link>
         </div>
-        <ul className='navLink'>
+        <ul className=''>
             <Link to='/Exploration'>Exploration</Link>
             <Link to='/Recomendation'>Recomendation</Link>
             <Link to='/About'>About</Link>
         </ul>
         <div>
-            <button className='getStarted'>Get Started <HiOutlineArrowUpRight/></button>
+            <button className=''>Get Started <HiOutlineArrowUpRight/></button>
         </div>
     </div>
   )
