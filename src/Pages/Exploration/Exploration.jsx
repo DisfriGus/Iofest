5b9db4ab984d398089c0cd61457bd81a442c3669
Jan Falih Fadhillah <bosspulsa57@gmail.com>
2023-05-02 23:02:36 +0700

    Fix aall

--- src/Pages/Exploration/Exploration.jsx
+++ src/Pages/Exploration/Exploration.jsx
@@ -1,68 +1,57 @@
-import React from 'react'
-import Navbar from '../../Component/Navbar/Navbar'
-import Section from '../../Component/Section'
+import React from "react";
+import Navbar from "../../Component/Navbar/Navbar";
+import Section from "../../Component/Section";
 import Card from "../../Component/Card";
-import Footer from '../../Component/Footer/Footer';
-
+import Footer from "../../Component/Footer/Footer";
+import destinations from "../../Data/Destination.json";
 const Exploration = () => {
   return (
-    <div className=''>
-      <Navbar/>
-      <div className='flex flex-col'>
+    <div className="">
+      <Navbar />
+      <div className="flex flex-col">
         <Section>
-          <div className='flex items-center flex-col w-full sm:pt-[72px] bg-bgExploration bg-no-repeat text-white pb-[8%] rounded-3xl  max-sm:bg-none' >
-              <h1 className='heading1 text-white sm:mb-4  max-sm:hidden'>Lets Explore</h1>
-              <p className='subheading mb-[88px] text-white max-sm:hidden'>Search any destination you prefer.</p>
-              <div className='flex items-center gap-12 max-sm:max-w-none max-sm:gap-0 max-sm:items-center'>
-                <input type="text" placeholder='Lets Explore' className='w-[760px] bg-transparent border-b-2 border-white outline-none max-sm:w-[200px] max-sm:bg-[#F7F8FB]w max-sm:text-black' />
-                <button className='py-4 px-[52px] bg-[#2257DF] rounded-lg max-sm:px-[18px]'>Search</button>
-              </div>
+          <div className="flex items-center flex-col w-full sm:pt-[72px] bg-bgExploration bg-no-repeat text-white pb-[8%] rounded-3xl  max-sm:bg-none">
+            <h1 className="heading1 text-white sm:mb-4  max-sm:hidden">
+              Lets Explore
+            </h1>
+            <p className="subheading mb-[88px] text-white max-sm:hidden">
+              Search any destination you prefer.
+            </p>
+            <div className="flex items-center gap-12 max-sm:max-w-none max-sm:gap-0 max-sm:items-center">
+              <input
+                type="text"
+                placeholder="Lets Explore"
+                className="w-[760px] bg-transparent border-b-2 border-white outline-none max-sm:w-[200px] max-sm:bg-[#F7F8FB]w max-sm:text-black"
+              />
+              <button className="py-4 px-[52px] bg-[#2257DF] rounded-lg max-sm:px-[18px]">
+                Search
+              </button>
+            </div>
           </div>
         </Section>
-        <Section title = "Popular Search" useArrows className="max-sm:hidden" >
-        <Card
-          type="destination"
-          to="/"
-          title="Discover New Experiences"
-          place="Istanbul, Turkey"
-          rating="4.5"        
-        />
-        <Card
-          type="destination"
-          to="/"
-          title="Discover New Experiences"
-          place="Istanbul, Turkey"
-          rating="4.5"
-        />
-        <Card
-          type="destination"
-          to="/"
-          title="Discover New Experiences"
-          place="Istanbul, Turkey"
-          rating="4.5"
-        />
-        <Card
-          type="destination"
-          to="/"
-          title="Discover New Experiences"
-          place="Istanbul, Turkey"
-          rating="4.5"
-        />
+        <Section title="Popular Destination" useArrows>
+          {destinations.destination.map((val) => (
+            <Card
+              type="destination"
+              to={`/place/${val.id}`}
+              src={val.image.url}
+              title={val.title}
+              place={val.place}
+              rating={val.rating}
+            />
+          ))}
         </Section>
-        <Section title="Popular Search" className='py-0 mt-[-80px] flex flex-col px-10  sm:hidden'>
-          
-        </Section>
-        <div className='flex flex-col gap-[10px] mt-4 pl-10 mb-[] '>
-            <p className='border-b-2'>Florence, Italy</p>
-            <p className='border-b-2'>Istanbul, Turkey</p>
-            <p className='border-b-2'>Komodo Island, Indoenesia</p>
-          </div>
+        <div className="flex flex-col max-sm:block hidden gap-[10px] mt-4 pl-10 mb-[] ">
+          <p className="border-b-2">Florence, Italy</p>
+          <p className="border-b-2">Istanbul, Turkey</p>
+          <p className="border-b-2">Komodo Island, Indoenesia</p>
+        </div>
       </div>
-      <div className='max-sm:mt-40'>
-        <Footer/>
+      <div className="max-sm:mt-40">
+        <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Exploration
+export default Exploration;
