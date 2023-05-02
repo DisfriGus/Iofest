9235d43030f2e76a1eb6b217ae5100ba9af2427a
Jan Falih Fadhillah <bosspulsa57@gmail.com>
2023-05-02 23:32:14 +0700

    fix some on blog

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Menjelajah from './Pages/Home/Menjelajah';
 import Exploration from './Pages/Exploration/Exploration';
-import Recomendation from './Pages/Recomendation/Recomendation';
-import About from './Pages/About/About';
 import Blog from './Pages/Blog/Blog';
 import SearchDetail from './Pages/SearchDetail';
 
@@ -12,9 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<div><Menjelajah/></div>}/>
         <Route path='/Exploration' element={<div><Exploration/></div>}/>
-        <Route path='/Recomendation' element={<div><Recomendation/></div>}/>
         <Route path='/Blog' element={<div><Blog/></div>}/>
-        <Route path='/About' element={<div><About/></div>}/>
         <Route path='/place/:id' element={<SearchDetail />}/>
       </Routes>
     </BrowserRouter>
