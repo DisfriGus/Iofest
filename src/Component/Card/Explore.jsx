08c72122d47ac16f1a50055ea02ccd30cc361976
DisfriGus <dio.siregar03@gmail.com>
2023-05-02 20:41:21 +0700

    responsive/hero

--- src/Component/Card/Explore.jsx
+++ src/Component/Card/Explore.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Explore = props => {
-    const {title, to, description} = props;
+    const {title, to, description,  className} = props;
     return(
-     <Link to={to} className='flex flex-col flex-1 group hover:cursor-pointer max-sm:items-center'>
+     <Link to={to} className={`${className} flex flex-col flex-1 group hover:cursor-pointer max-sm:items-center`}>
         <img className='w-full h-[240px] rounded-md bg-red-300 mb-4 object-cover' src='https://images.unsplash.com/photo-1682270251135-b917816669e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60' />
         <h3 className='heading4 group-hover:text-[#2257DF] mb-2 max-sm:title3'>{title}</h3>
         <p className='max-sm:text-center'>{description}</p>
--- src/Pages/Exploration/Exploration.jsx
+++ src/Pages/Exploration/Exploration.jsx
@@ -6,26 +6,26 @@ import Footer from '../../Component/Footer/Footer';
 
 const Exploration = () => {
   return (
-    <div>
+    <div className=''>
       <Navbar/>
       <div className='flex flex-col'>
         <Section>
-          <div className='flex items-center flex-col w-full sm:pt-[72px] bg-bgExploration bg-no-repeat text-white pb-[8%] rounded-3xl' >
-              <h1 className='heading1 text-white sm:mb-4 '>Lets Explore</h1>
-              <p className='subheading mb-[88px] text-white'>Search any destination you prefer.</p>
-              <div className='flex items-center gap-12'>
-                <input type="text" placeholder='Lets Explore' className='w-[760px] bg-transparent border-b-2 border-white outline-none' />
-                <button className='py-4 px-[52px] bg-[#2257DF] rounded-lg'>Search</button>
+          <div className='flex items-center flex-col w-full sm:pt-[72px] bg-bgExploration bg-no-repeat text-white pb-[8%] rounded-3xl  max-sm:bg-none' >
+              <h1 className='heading1 text-white sm:mb-4  max-sm:hidden'>Lets Explore</h1>
+              <p className='subheading mb-[88px] text-white max-sm:hidden'>Search any destination you prefer.</p>
+              <div className='flex items-center gap-12 max-sm:max-w-none max-sm:gap-0 max-sm:items-center'>
+                <input type="text" placeholder='Lets Explore' className='w-[760px] bg-transparent border-b-2 border-white outline-none max-sm:w-[200px] max-sm:bg-[#F7F8FB]w max-sm:text-black' />
+                <button className='py-4 px-[52px] bg-[#2257DF] rounded-lg max-sm:px-[18px]'>Search</button>
               </div>
           </div>
         </Section>
-        <Section title = "Popular Search" useArrows>
+        <Section title = "Popular Search" useArrows className="max-sm:hidden" >
         <Card
           type="destination"
           to="/"
           title="Discover New Experiences"
           place="Istanbul, Turkey"
-          rating="4.5"
+          rating="4.5"        
         />
         <Card
           type="destination"
@@ -49,8 +49,18 @@ const Exploration = () => {
           rating="4.5"
         />
         </Section>
+        <Section title="Popular Search" className='py-0 mt-[-80px] flex flex-col px-10  sm:hidden'>
+          
+        </Section>
+        <div className='flex flex-col gap-[10px] mt-4 pl-10 mb-[] '>
+            <p className='border-b-2'>Florence, Italy</p>
+            <p className='border-b-2'>Istanbul, Turkey</p>
+            <p className='border-b-2'>Komodo Island, Indoenesia</p>
+          </div>
       </div>
-      <Footer/>
+      <div className='max-sm:mt-40'>
+        <Footer/>
+      </div>
     </div>
   )
 }
