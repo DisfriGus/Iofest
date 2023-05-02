df9e561f5d78a429f695978b34cfd1c8fa8fb397
DisfriGus <dio.siregar03@gmail.com>
2023-05-02 18:40:31 +0700

    responsive/hero

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import Logo from  "../../Assets/_logo/_logo/_logo_1x.webp"
 
 const Navbar = () => {
   return (
+
     <div className='flex fixed z-10 bg-white w-full items-center justify-between px-20 max-sm:px-4 py-5'>
         <div className='flex max-sm:w-full max-sm:relative max-sm:items-center max-sm:justify-center'>
             <Link to='/' className=''>
@@ -32,7 +33,6 @@ const Navbar = () => {
         </ul>
         <div>
             <button className='flex items-center gap-[10px] px-6 py-3 border bg-[#161C2D] text-white rounded-[27px] max-sm:hidden'>Get Started <HiOutlineArrowUpRight/></button>
-          
         </div>
     </div>
   )
