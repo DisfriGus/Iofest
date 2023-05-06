5321c5a7e97ac64e712a6afe073cdbb916859bb3
DisfriGus <dio.siregar03@gmail.com>
2023-05-06 16:04:27 +0700

    feature/blog

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Navbar from '../../Component/Navbar/Navbar'
-import Section from '../../Component/Section'
-import Card from '../../Component/Card'
-import Footer from '../../Component/Footer/Footer'
-import Beach from '../../Assets/Rectangle 31.png'
+import React from "react";
+import Navbar from "../../Component/Navbar/Navbar";
+import Section from "../../Component/Section";
+import Card from "../../Component/Card";
+import Footer from "../../Component/Footer/Footer";
+import Beach from "../../Assets/Rectangle 31.png"
 
 const Blog = () => {
   return (
@@ -29,22 +29,51 @@ const Blog = () => {
                   title="4 days in Fukuoka, Japan: how to eat your way through the city"
                 />
               </div>
-            </div>
-            
-          </Section>
-          <Section title="Our Recommendation" className="w-1/3 max-sm:w-[333px]">
-              <div className='flex flex-col gap-10 '>
-                <Card type="hero"/>
-                <Card type="hero"/>
-                <Card type="hero"/>
-                <Card type="hero"/>
-                <Card type="hero"/>
-              </div>
-          </Section>
+          </div>
         </Section>
-        <Footer/>
+        <Section title="Our Recommendation" className="w-1/3">
+          <div className="flex flex-col gap-10">
+            <Card
+              type="hero"
+              minutes="5 MIN READ"
+              place="International"
+              title="5 Recommends places to go in 2023 as a couple"
+              description="Santorini, Greece: With its stunning views, romantic sunsets, and charming whitewashed buildings..."
+            />
+            <Card
+              type="hero"
+              minutes="10 MIN READ"
+              place="Local"
+              title="Welcome Heads of Delegation of the G20 Summit"
+              description="Malesuada vitae ante erat, donec gravida malesuada mus condimentum risus..."
+            />
+            <Card
+              type="hero"
+              minutes="12 MIN READ"
+              place="International"
+              title="4 Days in Fukuoka, Japan: how to eat your way through the city"
+              description="Fukuoka is known for its delicious food, and there are many local specialties that you should try during your four-day stay in the city..."
+            />
+            <Card
+              type="hero"
+              minutes="10 MIN READ"
+              place="Local"
+              title="7 Things to do in Bromo, Indonesia"
+              description="Bromo is a beautiful region in Indonesia with a variety of exciting activities and breathtaking landscapes. Here are seven things to do in..."
+            />
+            <Card
+              type="hero"
+              minutes="10 MIN READ"
+              place="Local"
+              title="7 Affordable Travel Destinations for Budget-Conscious Wanderers"
+              description="Thailand: Thailand is an affordable destination with a rich culture, beautiful beaches, and delicious food..."
+            />
+          </div>
+        </Section>
+      </Section>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
